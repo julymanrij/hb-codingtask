e098cbde087afeca7c6f57c479a459bce8c75ec4
Juliana Manrique Jimenez <julianamanrique@Julianas-MacBook-Pro.local>
2021-05-20 13:18:46 -0500

    remove route redirection

--- src/router.js
+++ src/router.js
@@ -31,7 +31,6 @@ const HelloBuildRouter = () => {
                   component={ListRepositories}
                 />
                 <Route path="/" exact component={Profile} />
-                <Route render={() => <Redirect to="/" />} />
               </Switch>
             </MainLayout>
           </Route>
@@ -41,7 +40,6 @@ const HelloBuildRouter = () => {
             <Switch>
               <Route path="/sign-up" exact component={SignUp} />
               <Route path="/" exact component={Login} />
-              <Route render={() => <Redirect to="/" />} />
             </Switch>
           </Route>
         )}
