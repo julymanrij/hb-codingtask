3bc8436f024f11fb01df36cba8018f61254842a2
Juliana Manrique Jimenez <julianamanrique@Julianas-MacBook-Pro.local>
2021-05-20 13:15:54 -0500

    change browserrouter to hashrouter

--- src/router.js
+++ src/router.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
+import { HashRouter as Router, Route, Switch, Redirect } from "react-router-dom";
 // Layouts
 import MainLayout from "./layouts/main";
 // Containers
